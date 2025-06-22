1d634112731a7905207885cc31bb49ffea89062a
harshil <devadharaharshil@gmil.com>
2025-06-22 16:21:48 +0530

    backend changes

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use('/api/questions', questionsRoute);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running at http://192.168.0.103:${PORT}`);
 });
+
